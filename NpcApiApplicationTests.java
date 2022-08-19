21728143cb9322100aeb17ba570fef993ff6e319
lingose123 <107252656+lingose123@users.noreply.github.com>
2022-08-19 17:39:48 +0800

    Add files via upload

---
+++ NpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
